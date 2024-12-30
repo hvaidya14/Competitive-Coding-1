4308e35cec5ae9447a01d66b821e49ae0e54943e
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2024-12-30 14:15:37 -0800

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,29 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
+class Main {
+    public static void main(String[] args) {
+        int a[] ={1,2,3,5,6,8};
+        
+        
+        int low = 0;
+        int high = a.length-1;
+        int mid=0;
+        
+        while (low <= high) {
+            mid = low + (high-low)/2;
+            int numberOfElements = mid - low;
+            if (high - low == 1) {
+                System.out.println("always");
+                System.out.println(a[low] + 1);
+                break;
+            }
+            if (a[mid] == (a[low] + numberOfElements)) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        
+    }
+}
